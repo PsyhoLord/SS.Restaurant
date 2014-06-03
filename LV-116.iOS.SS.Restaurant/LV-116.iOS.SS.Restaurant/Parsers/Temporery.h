f1333684006ec3860ce595945cb253a12be343c8
salomf <salomykola@gmail.com>
2014-06-03 15:34:44 +0300

    test_numb2
    byyyyyyyy

---
+++ LV-116.iOS.SS.Restaurant/LV-116.iOS.SS.Restaurant/Parsers/Temporery.h
@@ -0,0 +1,13 @@
+//
+//  Temporery.h
+//  LV-116.iOS.SS.Restaurant
+//
+//  Created by Administrator on 6/3/14.
+//  Copyright (c) 2014 SortServe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Temporery : NSObject
+
+@end
---
+++ LV-116.iOS.SS.Restaurant/LV-116.iOS.SS.Restaurant/Parsers/Temporery.m
@@ -0,0 +1,13 @@
+//
+//  Temporery.m
+//  LV-116.iOS.SS.Restaurant
+//
+//  Created by Administrator on 6/3/14.
+//  Copyright (c) 2014 SortServe. All rights reserved.
+//
+
+#import "Temporery.h"
+
+@implementation Temporery
+
+@end
