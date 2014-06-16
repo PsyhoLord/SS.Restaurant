00b775bbeb5bfaf7bda85fb3d2e34401c6bc473d
Mikola <Msalo@bogdan.training.local>
2014-06-16 19:21:23 +0300

    renamed temp value. remove coment

--- LV-116.iOS.SS.Restaurant/LV-116.iOS.SS.Restaurant/Controllers/MenuViewController.m
+++ LV-116.iOS.SS.Restaurant/LV-116.iOS.SS.Restaurant/Controllers/MenuViewController.m
@@ -26,8 +26,6 @@
 {
     self = [super initWithStyle:style];
     if (self) {
-        // Custom initialization
-        
     }
     return self;
 }
@@ -39,13 +37,13 @@
     _currentCategory = [_dataProvider getMenuData:nil];
     [self.MenuTableView reloadData];
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    [UIActivityIndicatorView setAnimationBeginsFromCurrentState:YES];
 }
 
 
 - (void)loadDataFromServer
 {
     _dataProvider = [[DataProvider alloc] init];
-    //    [_dataProvider setDelegate:self];
     [_dataProvider getMenuData:nil];
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
 }
@@ -61,12 +59,6 @@
         [self loadDataFromServer];
     
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
 }
 
 - (void)didReceiveMemoryWarning
@@ -79,14 +71,12 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    //#warning Potentially incomplete method implementation.
     // Return the number of sections.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    //#warning Incomplete method implementation.
     // Return the number of rows in the section.
     int q;
     if (self.currentCategory.items)
@@ -105,17 +95,17 @@
     if ( !cell ) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
     }
-    MenuCategory *tempmc=[[MenuCategory alloc]init];
+    MenuCategory *tempVariableForTitle=[[MenuCategory alloc]init];
     if (_currentCategory.categories)
     {
-        tempmc=[self.currentCategory.categories objectAtIndex:indexPath.row];
-        cell.textLabel.text=tempmc.name;
+        tempVariableForTitle=[self.currentCategory.categories objectAtIndex:indexPath.row];
+        cell.textLabel.text=tempVariableForTitle.name;
     }
     else
     {
-        tempmc=[self.currentCategory.items objectAtIndex:indexPath.row];
-        cell.textLabel.text=tempmc.name;
-        cell.detailTextLabel.text=tempmc.description;
+        tempVariableForTitle=[self.currentCategory.items objectAtIndex:indexPath.row];
+        cell.textLabel.text=tempVariableForTitle.name;
+        cell.detailTextLabel.text=tempVariableForTitle.description;
         didReachBottomMenuLevel = YES;
     }
     return cell;
@@ -145,56 +135,5 @@
         [self.navigationController pushViewController:vc animated:YES];
     }
 }
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-/*
- #pragma mark - Navigation
- 
- // In a story board-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
- {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- 
- */
 
 @end
