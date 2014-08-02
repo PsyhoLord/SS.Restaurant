ab1f3f91451ebf582fff7977534a3c00eb8cc92c
romansorochak <roman.sorochak@gmail.com>
2014-08-02 12:33:59 +0300

    Created class LocalServiceAgent.

---
+++ LV-116.iOS.SS.Restaurant/LV-116.iOS.SS.Restaurant/ServiceAgents/LocalServiceAgent/LocalServiceAgent.h
@@ -0,0 +1,13 @@
+//
+//  LocalServiceAgent.h
+//  LV-116.iOS.SS.Restaurant
+//
+//  Created by Roman Sorochak on 8/2/14.
+//  Copyright (c) 2014 SortServe. All rights reserved.
+//
+
+@interface LocalServiceAgent : NSObject
+
+
+
+@end
---
+++ LV-116.iOS.SS.Restaurant/LV-116.iOS.SS.Restaurant/ServiceAgents/LocalServiceAgent/LocalServiceAgent.m
@@ -0,0 +1,13 @@
+//
+//  LocalServiceAgent.m
+//  LV-116.iOS.SS.Restaurant
+//
+//  Created by Roman Sorochak on 8/2/14.
+//  Copyright (c) 2014 SortServe. All rights reserved.
+//
+
+#import "LocalServiceAgent.h"
+
+@implementation LocalServiceAgent
+
+@end
